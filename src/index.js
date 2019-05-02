2c1c4a1d8bb0fccdb62cdac7454bd8ee7f3268da
vklimov <vklimov@livemaster.ru>
2019-05-02 21:56:24 +0300

    tested fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world');
+const getResource = async (url) => {
+	const res = await fetch(url);
+
+	if (!res.ok) {
+		throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+	}
+
+	const body = await res.json();
+	return body;
+};
+
+getResource('https://swapi.co/api/people/1/')
+	.then(body => console.log(body))
+	.catch(err => console.error(err));
+
+
+// fetch('https://swapi.co/api/people/1/')
+// 	.then(res => {
+// 		return res.json();
+// 	})
+// 	.then(body => {
+// 		console.log(body);
+// 	})
+// 	.catch (err => {
+// 		console.log(err)
+// 	});
