63fdde6b15f7f22846ecd39526626ff5f65e6801
Viktor Klimov <v.klimov@tactise.com>
2019-10-29 03:50:58 +0300

    Updates

---
+++ precache-manifest.1f1805dda607b4909a43446a5dc0ab06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e95cda0b92944d6342f809142d7c1aa",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "bee8438d7072856a94f0",
+    "url": "/star-db/static/css/main.5c4a87bc.chunk.css"
+  },
+  {
+    "revision": "bb0c689ebd32573d7635",
+    "url": "/star-db/static/js/2.814abbd9.chunk.js"
+  },
+  {
+    "revision": "bee8438d7072856a94f0",
+    "url": "/star-db/static/js/main.46113280.chunk.js"
+  },
+  {
+    "revision": "5f739498edc2f2e50ad7",
+    "url": "/star-db/static/js/runtime~main.8f3510d5.js"
+  }
+]);
